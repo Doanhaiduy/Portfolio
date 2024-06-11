c5a593541f774b4bbc9ac3d2c5de492a5e6ccfcc
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-06-11 14:03:54 +0700

    change api to public

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-// const API_URL = 'https://portfolio-api-gamma-three.vercel.app';
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://portfolio-api-gamma-three.vercel.app';
+// const API_URL = 'http://localhost:5000';
 
 export { API_URL };
